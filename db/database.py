c9e9541834afe239ec530cf8e9becd8e9a50c908
Takács Bálint <tbalint99@gmail.com>
2022-11-08 23:37:29 +0100

    env var added to cloudinary integration

--- db/database.py
+++ db/database.py
@@ -2,11 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import cloudinary
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 cloudinary.config(
-    cloud_name="next-drinks",
-    api_key="989878735844422",
-    api_secret="rvIiw2yj5b7gDef90PllO8tz4B4"
+    cloud_name=os.getenv("CLOUD_NAME"),
+    api_key=os.getenv("API_KEY"),
+    api_secret=os.getenv("API_SECRET")
 )
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./intagram_api.db'
